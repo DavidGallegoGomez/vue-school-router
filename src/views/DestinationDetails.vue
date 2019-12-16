2c66c5c27b7856a34fa5f38701b0b094e5c7bedb
David Gallego Gomez <davizs@hotmail.com>
2019-12-17 00:01:11 +0100

    bugfix: Lesson 8

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -3,6 +3,7 @@
     <h1>{{ destination.name }}</h1>
     <div class="destination-details">
       <img :src="require(`@/assets/${destination.image}`)" :alt="destination.name">
+      <p>{{ destination.description }}</p>
     </div>
   </section>
 </template>
@@ -11,13 +12,35 @@
 import store from "@/data/store.js";
 
 export default {
-  data: () => ({
-    destinationId: this.$route.params.id
-  }),
+  data() {
+    return {
+      destinationId: this.$route.params.id
+    }
+  },
   computed: {
     destination() {
-      return store.destinations.find( destination => destination.id === this.destinationId)
+      return store.destinations.find(
+        destination => destination.id === this.destinationId
+      )
     }
   }
 }
 </script>
+
+<style scoped>
+  img {
+    max-width: 600px;
+    height: auto;
+    width: 100%;
+    max-height: 400px;
+  }
+  .destination-details {
+    display: flex;
+    justify-content: space-between;
+  }
+  p {
+    margin: 0 40px;
+    font-size: 20px;
+    text-align: left;
+  }
+</style>
